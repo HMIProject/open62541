ff8da4b3c5650770b9d37bd6e330672e5b19ed2f
Sebastian Goll <goll@hmi-project.com>
2023-11-27 23:30:43 +0100

    Add back check to confirm string allocation in node ID was successful

--- src/ua/data_types/node_id.rs
+++ src/ua/data_types/node_id.rs
@@ -12,7 +12,11 @@ impl NodeId {
     #[must_use]
     pub fn new_numeric(ns_index: u16, numeric: u32) -> Self {
         let inner = unsafe { UA_NODEID_NUMERIC(ns_index, numeric) };
-        debug_assert_eq!(inner.identifierType, UA_NodeIdType_UA_NODEIDTYPE_NUMERIC);
+        debug_assert_eq!(
+            inner.identifierType, UA_NodeIdType_UA_NODEIDTYPE_NUMERIC,
+            "node ID is of numeric type"
+        );
+
         Self(inner)
     }
 
@@ -20,14 +24,28 @@ impl NodeId {
     ///
     /// # Panics
     ///
-    /// The string identifier must be a valid C string, i.e. it must not contain any NUL bytes.
+    /// The string identifier must be a valid C string, i.e. it must not contain any NUL bytes. Also
+    /// there must be enough memory available to allocate string.
     #[must_use]
     pub fn new_string(ns_index: u16, string: &str) -> Self {
         let string = CString::new(string).expect("node ID string does not contain NUL bytes");
+
         // Technically, string allocation may fail but `UA_NODEID_STRING_ALLOC` doesn't tell us that
         // when it happens. Instead, we end up with a well-defined node ID that has an empty string.
         let inner = unsafe { UA_NODEID_STRING_ALLOC(ns_index, string.as_ptr()) };
-        debug_assert_eq!(inner.identifierType, UA_NodeIdType_UA_NODEIDTYPE_STRING);
+        debug_assert_eq!(
+            inner.identifierType, UA_NodeIdType_UA_NODEIDTYPE_STRING,
+            "node ID is of string type"
+        );
+
+        // SAFETY: We have checked that we have this enum variant.
+        let identifier = unsafe { inner.identifier.string };
+        if !string.is_empty() && (identifier.data.is_null() || identifier.length == 0) {
+            // We don't want to leak memory on top.
+            debug_assert!(identifier.data.is_null());
+            panic!("node ID string has been allocated");
+        }
+
         Self(inner)
     }
 }
