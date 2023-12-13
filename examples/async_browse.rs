76b1a0b10328dfa3a8653c531c00cdf2d4f9ed39
Sebastian Goll <goll@hmi-project.com>
2023-12-13 16:34:46 +0100

    Add example for asynchronous browsing

---
+++ examples/async_browse.rs
@@ -0,0 +1,23 @@
+use anyhow::Context;
+use open62541::{ua, AsyncClient};
+use open62541_sys::UA_NS0ID_SERVER_SERVERSTATUS;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let client =
+        AsyncClient::new("opc.tcp://opcuademo.sterfive.com:26543").with_context(|| "connect")?;
+
+    let node_id = ua::NodeId::new_numeric(0, UA_NS0ID_SERVER_SERVERSTATUS);
+
+    println!("Browsing node {node_id}");
+
+    let references = client.browse(&node_id).await?;
+
+    for reference in references {
+        println!("- {}", reference.browse_name());
+    }
+
+    Ok(())
+}
