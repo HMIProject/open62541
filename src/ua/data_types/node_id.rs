d0b87420d8129d2b6c07d929e68c4385f4d909ac
Sebastian Goll <goll@hmi-project.com>
2023-12-15 13:48:56 +0100

    Add string conversion and optional serde serialization for node IDs

--- src/ua/data_types/node_id.rs
+++ src/ua/data_types/node_id.rs
@@ -1,8 +1,8 @@
-use std::ffi::CString;
+use std::{ffi::CString, str};
 
-use open62541_sys::{UA_NodeIdType, UA_NODEID_NUMERIC, UA_NODEID_STRING_ALLOC};
+use open62541_sys::{UA_NodeIdType, UA_NodeId_parse, UA_NODEID_NUMERIC, UA_NODEID_STRING_ALLOC};
 
-use crate::ua;
+use crate::{data_type::DataType, ua, Error};
 
 crate::data_type!(NodeId, UA_NodeId, UA_TYPES_NODEID);
 
@@ -14,7 +14,7 @@ impl NodeId {
         debug_assert_eq!(
             inner.identifierType,
             UA_NodeIdType::UA_NODEIDTYPE_NUMERIC,
-            "node ID is of numeric type"
+            "new node ID should have numeric type"
         );
 
         Self(inner)
@@ -24,8 +24,7 @@ impl NodeId {
     ///
     /// # Panics
     ///
-    /// The string identifier must be a valid C string, i.e. it must not contain any NUL bytes. Also
-    /// there must be enough memory available to allocate string.
+    /// The string identifier must not contain any NUL bytes.
     #[must_use]
     pub fn string(ns_index: u16, string: &str) -> Self {
         let string = CString::new(string).expect("node ID string does not contain NUL bytes");
@@ -36,7 +35,7 @@ impl NodeId {
         debug_assert_eq!(
             inner.identifierType,
             UA_NodeIdType::UA_NODEIDTYPE_STRING,
-            "node ID is of string type"
+            "new node ID should have string type"
         );
 
         // SAFETY: We have checked that we have this enum variant.
@@ -44,7 +43,7 @@ impl NodeId {
         if !string.is_empty() && (identifier.data.is_null() || identifier.length == 0) {
             // We don't want to leak memory on top.
             debug_assert!(identifier.data.is_null());
-            panic!("node ID string has been allocated");
+            panic!("node ID string should have been allocated");
         }
 
         Self(inner)
@@ -53,5 +52,115 @@ impl NodeId {
     #[must_use]
     pub fn identifier_type(&self) -> ua::NodeIdType {
         ua::NodeIdType::new(self.0.identifierType.clone())
+    }
+}
+
+impl str::FromStr for NodeId {
+    type Err = Error;
+
+    /// ```
+    /// use open62541::ua;
+    ///
+    /// // Valid node IDs can be parsed.
+    /// let node_id: ua::NodeId = "ns=0;i=2258".parse().expect("should be valid node ID");
+    ///
+    /// // Node IDs are normalized (note that `ns=0` has been dropped).
+    /// assert_eq!(node_id.to_string(), "i=2258");
+    ///
+    /// // Parsing node IDs can fail.
+    /// "LoremIpsum".parse::<ua::NodeId>().expect_err("should be invalid node ID");
+    /// ```
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut node_id = NodeId::init();
+
+        let status_code = ua::StatusCode::new({
+            let str: ua::String = s.parse()?;
+            let id = node_id.as_mut_ptr();
+            let str = str.into_inner();
+            unsafe { UA_NodeId_parse(id, str) }
+        });
+        Error::verify_good(status_code)?;
+
+        Ok(node_id)
+    }
+}
+
+#[cfg(feature = "serde")]
+mod serde {
+    use std::fmt;
+
+    use super::NodeId;
+
+    impl serde::Serialize for NodeId {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer,
+        {
+            serializer.serialize_str(&self.to_string())
+        }
+    }
+
+    impl<'de> serde::Deserialize<'de> for NodeId {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            deserializer.deserialize_str(NodeIdVisitor)
+        }
+    }
+
+    struct NodeIdVisitor;
+
+    impl<'de> serde::de::Visitor<'de> for NodeIdVisitor {
+        type Value = NodeId;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+            formatter.write_str("an OPC UA node ID")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            v.parse()
+                .map_err(|_| serde::de::Error::custom("invalid node ID"))
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::ua;
+
+        #[test]
+        fn json_serialization() {
+            let node_id: ua::NodeId =
+                serde_json::from_str(r#""ns=0;i=2258""#).expect("should deserialize node ID");
+
+            assert_eq!(
+                serde_json::to_string(&node_id).expect("should serialize node ID"),
+                r#""i=2258""#
+            );
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str;
+
+    use crate::ua;
+
+    #[test]
+    fn string_representation() {
+        // We explicitly derive `FromStr` and `ToString`. This is part of the public interface.
+        //
+        let node_id =
+            <ua::NodeId as str::FromStr>::from_str("ns=0;i=2258").expect("should be valid node ID");
+
+        assert_eq!(<ua::NodeId as ToString>::to_string(&node_id), "i=2258");
+
+        // Usually, parsing is done via `parse()` however which is implemented for `FromStr` target.
+        //
+        let _node_id: ua::NodeId = "ns=0;i=2258".parse().expect("should be valid node ID");
     }
 }
