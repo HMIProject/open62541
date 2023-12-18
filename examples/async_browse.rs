a41569d86f721d90c196940ab06f2cae78905064
Sebastian Goll <goll@hmi-project.com>
2023-12-18 18:05:02 +0100

    Add demonstration of deep efficient browsing of OPC UA server

--- examples/async_browse.rs
+++ examples/async_browse.rs
@@ -1,6 +1,8 @@
+use std::collections::{hash_map::Entry, HashMap, VecDeque};
+
 use anyhow::Context;
 use open62541::{ua, AsyncClient};
-use open62541_sys::UA_NS0ID_SERVER_SERVERSTATUS;
+use open62541_sys::UA_NS0ID_SERVERTYPE;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
@@ -9,21 +11,178 @@ async fn main() -> anyhow::Result<()> {
     let client =
         AsyncClient::new("opc.tcp://opcuademo.sterfive.com:26543").with_context(|| "connect")?;
 
-    let node_ids = [
-        ua::NodeId::numeric(0, UA_NS0ID_SERVER_SERVERSTATUS),
-        // `/Root/Objects/1:Boiler#1/1:Simulation`
-        ua::NodeId::numeric(1, 1801),
-    ];
+    let hierarchy = browse_hierarchy(&client, &ua::NodeId::numeric(0, UA_NS0ID_SERVERTYPE)).await?;
 
-    for node_id in node_ids {
-        println!("Browsing node {node_id}");
+    hierarchy.pretty_print(|name, (node_id, node)| {
+        let Some(name) = name else {
+            return format!("{node_id}");
+        };
 
-        let references = client.browse(&node_id).await.with_context(|| "browse")?;
+        let info = vec![
+            node.display_name().text().to_string().into_owned(),
+            node.node_class().to_string(),
+        ];
 
-        for reference in references {
-            println!("- {}", reference.browse_name().to_string());
+        let info = info.join(", ");
+
+        format!("{name} ({info}) -> {node_id}")
+    });
+
+    Ok(())
+}
+
+/// Maximum number of node IDs to browse in single request.
+const MAX_NODE_IDS: usize = 1000;
+
+async fn browse_hierarchy(
+    client: &AsyncClient,
+    root_node_id: &ua::NodeId,
+) -> anyhow::Result<TreeNode<(ua::NodeId, ua::ReferenceDescription)>> {
+    // Maps parent node IDs to list of children (node IDs with their respective names).
+    let mut children: HashMap<ua::NodeId, Vec<ua::ReferenceDescription>> = HashMap::new();
+
+    let mut pending_node_ids = VecDeque::new();
+    pending_node_ids.push_back(root_node_id.clone());
+
+    while !pending_node_ids.is_empty() {
+        let browse_node_ids: Vec<_> = pending_node_ids
+            .drain(..MAX_NODE_IDS.min(pending_node_ids.len()))
+            .collect();
+
+        println!(
+            "Browsing {} node IDs {browse_node_ids:?}",
+            browse_node_ids.len()
+        );
+
+        let results = client
+            .browse_many(&browse_node_ids)
+            .await
+            .with_context(|| "browse")?;
+
+        for (node_id, result) in browse_node_ids.into_iter().zip(results) {
+            let children = match children.entry(node_id) {
+                Entry::Occupied(child) => {
+                    let node_id = child.key();
+                    println!("Ignoring child `{node_id}` reachable on different paths");
+                    continue;
+                }
+                Entry::Vacant(entry) => entry.insert(Vec::new()),
+            };
+
+            let Some(references) = result else {
+                continue;
+            };
+
+            for reference in references {
+                pending_node_ids.push_back(reference.node_id().node_id());
+
+                children.push(reference);
+            }
         }
     }
 
-    Ok(())
+    let root_node = (root_node_id.clone(), ua::ReferenceDescription::init());
+
+    let tree_node = TreeNode::build_tree(root_node, move |(node_id, _)| {
+        children
+            .remove(node_id)
+            .unwrap_or_else(|| {
+                println!("Assuming no children for unbrowsed child `{node_id}`");
+                Vec::new()
+            })
+            .into_iter()
+            .map(|browse_description| {
+                (
+                    browse_description.browse_name().to_string(),
+                    (browse_description.node_id().node_id(), browse_description),
+                )
+            })
+    });
+
+    Ok(tree_node)
+}
+
+#[derive(Debug)]
+struct TreeNode<T> {
+    value: T,
+    children: HashMap<String, TreeNode<T>>,
+}
+
+impl<T> TreeNode<T> {
+    /// Builds tree from root using child list callback.
+    fn build_tree<FC, C>(root_value: T, mut children: FC) -> Self
+    where
+        FC: FnMut(&T) -> C,
+        C: Iterator<Item = (String, T)>,
+    {
+        let mut root_node = TreeNode {
+            value: root_value,
+            children: HashMap::new(),
+        };
+
+        let mut pending_nodes = vec![&mut root_node];
+
+        while let Some(node) = pending_nodes.pop() {
+            debug_assert!(node.children.is_empty());
+
+            for (child_name, child_value) in children(&node.value) {
+                let child_node = TreeNode {
+                    value: child_value,
+                    children: HashMap::new(),
+                };
+
+                match node.children.entry(child_name) {
+                    Entry::Occupied(entry) => {
+                        println!("Ignoring duplicate child with name `{}`", entry.key());
+                    }
+                    Entry::Vacant(entry) => {
+                        entry.insert(child_node);
+                    }
+                }
+            }
+
+            pending_nodes.extend(node.children.values_mut());
+        }
+
+        root_node
+    }
+
+    fn pretty_print<FL>(&self, mut label: FL)
+    where
+        FL: FnMut(Option<&str>, &T) -> String,
+    {
+        struct PrintNode<'a, T> {
+            name: Option<&'a str>,
+            node: &'a TreeNode<T>,
+            level: usize,
+        }
+
+        let mut pending_nodes = VecDeque::new();
+        pending_nodes.push_back(PrintNode {
+            name: None,
+            node: self,
+            level: 0,
+        });
+
+        while let Some(node) = pending_nodes.pop_back() {
+            let has_children = !node.node.children.is_empty();
+            let prefix = "  ".repeat(node.level) + (if has_children { "+" } else { "-" }) + " ";
+
+            let label = label(node.name, &node.node.value);
+
+            println!("{prefix}{label}");
+
+            let mut children: Vec<(&String, &TreeNode<T>)> = node.node.children.iter().collect();
+
+            children.sort_by_key(|&(child_name, _)| child_name);
+
+            for (child_name, child_node) in children.into_iter().rev() {
+                pending_nodes.push_back(PrintNode {
+                    name: Some(child_name),
+                    node: child_node,
+                    level: node.level + 1,
+                });
+            }
+        }
+    }
 }
--- src/ua/data_types/localized_text.rs
+++ src/ua/data_types/localized_text.rs
@@ -1,1 +1,15 @@
+use crate::ua;
+
 crate::data_type!(LocalizedText, UA_LocalizedText, UA_TYPES_LOCALIZEDTEXT);
+
+impl LocalizedText {
+    #[must_use]
+    pub fn locale(&self) -> ua::String {
+        ua::String::from_ref(&self.0.locale)
+    }
+
+    #[must_use]
+    pub fn text(&self) -> ua::String {
+        ua::String::from_ref(&self.0.text)
+    }
+}
