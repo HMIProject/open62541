0565ef52cfe3c488b80719b9c5aaa95f0f35f1f2
Sebastian Goll <1277035+sgoll@users.noreply.github.com>
2024-02-28 18:53:57 +0100

    Include predefined variants for NodeClass enum data type (#48)
    ## Description
    
    This adds the known variants to the wrapped `ua::NodeClass` enum data
    type.

--- src/ua/data_types/node_class.rs
+++ src/ua/data_types/node_class.rs
@@ -1,1 +1,17 @@
 crate::data_type!(NodeClass);
+
+crate::enum_variants!(
+    NodeClass,
+    UA_NodeClass,
+    [
+        UNSPECIFIED,
+        OBJECT,
+        VARIABLE,
+        METHOD,
+        OBJECTTYPE,
+        VARIABLETYPE,
+        REFERENCETYPE,
+        DATATYPE,
+        VIEW,
+    ],
+);
