2cf1eea218e35392039680139bf7e184e6509958
Sebastian Goll <goll@hmi-project.com>
2024-02-29 23:23:44 +0100

    Add todo reminder to browse example

--- examples/async_browse.rs
+++ examples/async_browse.rs
@@ -66,7 +66,8 @@ async fn browse_hierarchy(
                 Entry::Vacant(entry) => entry.insert(Vec::new()),
             };
 
-            let Some((references, _)) = result else {
+            // TODO: Use continuation point to fetch remaining references if necessary.
+            let Some((references, _continuation_point)) = result else {
                 continue;
             };
 
