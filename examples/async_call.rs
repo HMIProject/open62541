722da528269c989b70a1c2cc229dc28cefc40ea3
Sebastian Goll <goll@hmi-project.com>
2024-03-07 17:51:32 +0100

    Include example how to fetch method definitions (wip)

--- examples/async_call.rs
+++ examples/async_call.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Context as _};
 use open62541::{ua, AsyncClient, DataType as _};
+use open62541_sys::{UA_NS0ID_HASPROPERTY, UA_NS0ID_PROPERTYTYPE};
 
 const CYCLE_TIME: tokio::time::Duration = tokio::time::Duration::from_millis(100);
 
@@ -49,6 +50,19 @@ async fn call_method(
     method_node_id: &ua::NodeId,
     input_arguments: &[ua::Variant],
 ) -> anyhow::Result<Option<Vec<ua::Variant>>> {
+    println!("Getting method definition of node {method_node_id}");
+
+    let definition = get_definition(client, method_node_id).await?;
+
+    println!(
+        "- input arguments: {:?}",
+        definition.input_arguments.unwrap_or_default()
+    );
+    println!(
+        "- output arguments: {:?}",
+        definition.output_arguments.unwrap_or_default()
+    );
+
     println!("Calling node {method_node_id}");
 
     let output_arguments = client
@@ -59,4 +73,84 @@ async fn call_method(
     println!("-> {output_arguments:?}");
 
     Ok(output_arguments)
+}
+
+#[derive(Debug)]
+struct MethodDefinition {
+    // TODO: Return `ScalarValueType` or similar instead of raw node ID for argument type.
+    input_arguments: Option<Vec<(ua::String, ua::NodeId)>>,
+    output_arguments: Option<Vec<(ua::String, ua::NodeId)>>,
+}
+
+const INPUT_ARGUMENTS_PROPERTY_NAME: &'static str = "InputArguments";
+const OUTPUT_ARGUMENTS_PROPERTY_NAME: &'static str = "OutputArguments";
+
+async fn get_definition(
+    client: &AsyncClient,
+    method_node_id: &ua::NodeId,
+) -> anyhow::Result<MethodDefinition> {
+    let (references, _) = client.browse(method_node_id).await?;
+
+    let mut input_arguments = None;
+    let mut output_arguments = None;
+
+    for reference in &references {
+        match property_name(reference) {
+            Some(INPUT_ARGUMENTS_PROPERTY_NAME) => input_arguments = Some(reference.node_id()),
+            Some(OUTPUT_ARGUMENTS_PROPERTY_NAME) => output_arguments = Some(reference.node_id()),
+            _ => {}
+        }
+    }
+
+    // TODO: Refactor. Query input/output argument definitions in single request.
+    let input_arguments = match input_arguments {
+        Some(arguments) => Some(get_arguments(client, arguments.node_id()).await?),
+        None => None,
+    };
+    let output_arguments = match output_arguments {
+        Some(arguments) => Some(get_arguments(client, arguments.node_id()).await?),
+        None => None,
+    };
+
+    Ok(MethodDefinition {
+        input_arguments,
+        output_arguments,
+    })
+}
+
+async fn get_arguments(
+    client: &AsyncClient,
+    arguments: &ua::NodeId,
+) -> anyhow::Result<Vec<(ua::String, ua::NodeId)>> {
+    let arguments = client.read_value(arguments).await?;
+
+    let arguments = arguments
+        .value()
+        .ok_or(anyhow::anyhow!("should have value"))?
+        .to_array::<ua::ExtensionObject>()
+        .ok_or(anyhow::anyhow!("should have array"))?;
+
+    arguments
+        .iter()
+        .map(|object| {
+            let argument = object
+                .decoded_content::<ua::Argument>()
+                .ok_or(anyhow::anyhow!("should have argument"))?;
+            Ok((argument.name().clone(), argument.data_type().clone()))
+        })
+        .collect()
+}
+
+/// Gets name of referenced property.
+fn property_name(reference: &ua::ReferenceDescription) -> Option<&str> {
+    (reference.node_class() == &ua::NodeClass::VARIABLE
+        && reference.reference_type_id().as_ns0() == Some(UA_NS0ID_HASPROPERTY)
+        && reference.type_definition().node_id().as_ns0() == Some(UA_NS0ID_PROPERTYTYPE))
+    .then(|| {
+        reference
+            .browse_name()
+            .as_ns0()
+            .and_then(|name| name.as_str())
+    })
+    .flatten()
 }
