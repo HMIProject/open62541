1c389c394a3d2efd1880f8b72e18a9385220bf49
Sebastian Goll <goll@hmi-project.com>
2024-04-10 15:10:29 +0200

    Clean up Rustdoc comments in `Server`

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,9 @@
 use crate::{ua, DataType, Error, ObjectNode, Result, VariableNode};
 
+/// OPC UA server.
+///
+/// This represents an OPC UA server. Nodes can be added through the several methods below, and then
+/// the server can be started with [`run()`](Self::run).
 pub struct Server(ua::Server);
 
 impl Server {
@@ -9,13 +13,11 @@ impl Server {
         Self(ua::Server::new())
     }
 
-    ///
-    /// Adds a new variable to the address space
+    /// Adds variable to address space.
     ///
     /// # Errors
     ///
-    /// When a error happens adding the node
-    ///
+    /// This fails when the node cannot be added.
     pub fn add_variable_node(&mut self, node: VariableNode) -> Result<()> {
         let status_code = self.0.add_variable_node(
             node.requested_new_node_id,
@@ -28,13 +30,11 @@ impl Server {
         Error::verify_good(&status_code)
     }
 
-    ///
-    /// Adds a new object to the address space
+    /// Adds object to address space.
     ///
     /// # Errors
     ///
-    /// When a error happens adding the node
-    ///
+    /// This fails when the node cannot be added.
     pub fn add_object_node(&mut self, node: ObjectNode) -> Result<()> {
         let status_code = self.0.add_object_node(
             node.requested_new_node_id,
@@ -47,25 +47,38 @@ impl Server {
         Error::verify_good(&status_code)
     }
 
-    ///
-    /// Writes a value to a variable
+    /// Writes value to variable.
     ///
     /// # Errors
     ///
-    /// When a error happens writing the value
-    ///
+    /// This fails when the variable cannot be written.
     pub fn write_variable(&mut self, node_id: ua::NodeId, value: ua::Variant) -> Result<()> {
         let status_code = self.0.write_variable(node_id, value);
         Error::verify_good(&status_code)
     }
 
+    /// Writes string to variable.
     ///
-    /// Writes a string to a variable
+    /// This is a shortcut and roughly equivalent to the following:
+    ///
+    /// ```
+    /// # use open62541::{ua, DataType as _, Server};
+    /// #
+    /// # fn write_string(
+    /// #     server: &mut Server,
+    /// #     node_id: ua::NodeId,
+    /// #     value: &str,
+    /// # ) -> anyhow::Result<()> {
+    /// let value = ua::String::new(value)?;
+    /// let value = ua::Variant::init().with_scalar(&value);
+    /// server.write_variable(node_id, value)?;
+    /// # Ok(())
+    /// # }
+    /// ```
     ///
     /// # Errors
     ///
-    /// When a error happens writing the value
-    ///
+    /// This fails when the variable cannot be written.
     pub fn write_variable_string(&mut self, node_id: ua::NodeId, value: &str) -> Result<()> {
         let value = ua::String::new(value)?;
         let ua_variant = ua::Variant::init().with_scalar(&value);
@@ -77,7 +90,6 @@ impl Server {
     /// # Errors
     ///
     /// When an error occurred internally
-    ///
     pub fn run(self) -> Result<()> {
         let status_code = self.0.run_until_interrupt();
         Error::verify_good(&status_code)
