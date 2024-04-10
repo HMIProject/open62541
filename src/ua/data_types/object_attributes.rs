21d431610a882a2c7723ab5a83f1a260291042e1
Sebastian Goll <goll@hmi-project.com>
2024-04-10 15:10:54 +0200

    Avoid custom clone logic, let open62541 handle this

--- src/ua/data_types/object_attributes.rs
+++ src/ua/data_types/object_attributes.rs
@@ -1,29 +1,9 @@
-use crate::{ua, DataType};
+use crate::DataType as _;
 
 crate::data_type!(ObjectAttributes);
 
-impl ObjectAttributes {}
-
 impl Default for ObjectAttributes {
     fn default() -> Self {
-        let attrs = unsafe { &open62541_sys::UA_ObjectAttributes_default };
-
-        Self(ObjectAttributes::copy_ua_object_attributes(attrs))
-    }
-}
-
-impl ObjectAttributes {
-    #[must_use]
-    pub fn copy_ua_object_attributes(
-        attrs: &open62541_sys::UA_ObjectAttributes,
-    ) -> open62541_sys::UA_ObjectAttributes {
-        open62541_sys::UA_ObjectAttributes {
-            specifiedAttributes: attrs.specifiedAttributes,
-            displayName: ua::LocalizedText::clone_raw(&attrs.displayName).into_raw(),
-            description: ua::LocalizedText::clone_raw(&attrs.description).into_raw(),
-            writeMask: attrs.writeMask,
-            userWriteMask: attrs.userWriteMask,
-            eventNotifier: attrs.eventNotifier,
-        }
+        Self::clone_raw(unsafe { &open62541_sys::UA_ObjectAttributes_default })
     }
 }
--- src/ua/data_types/variable_attributes.rs
+++ src/ua/data_types/variable_attributes.rs
@@ -1,37 +1,9 @@
-use crate::{ua, DataType};
+use crate::DataType as _;
 
 crate::data_type!(VariableAttributes);
 
-impl VariableAttributes {}
-
 impl Default for VariableAttributes {
     fn default() -> Self {
-        let attrs = unsafe { &open62541_sys::UA_VariableAttributes_default };
-
-        Self(VariableAttributes::copy_ua_variable_attributes(attrs))
-    }
-}
-
-impl VariableAttributes {
-    #[must_use]
-    pub fn copy_ua_variable_attributes(
-        attrs: &open62541_sys::UA_VariableAttributes,
-    ) -> open62541_sys::UA_VariableAttributes {
-        open62541_sys::UA_VariableAttributes {
-            specifiedAttributes: attrs.specifiedAttributes,
-            displayName: ua::LocalizedText::clone_raw(&attrs.displayName).into_raw(),
-            description: ua::LocalizedText::clone_raw(&attrs.description).into_raw(),
-            writeMask: attrs.writeMask,
-            userWriteMask: attrs.userWriteMask,
-            value: ua::Variant::clone_raw(&attrs.value).into_raw(),
-            dataType: ua::NodeId::clone_raw(&attrs.dataType).into_raw(),
-            valueRank: attrs.valueRank,
-            arrayDimensionsSize: attrs.arrayDimensionsSize,
-            arrayDimensions: attrs.arrayDimensions,
-            accessLevel: attrs.accessLevel,
-            userAccessLevel: attrs.userAccessLevel,
-            minimumSamplingInterval: attrs.minimumSamplingInterval,
-            historizing: attrs.historizing,
-        }
+        Self::clone_raw(unsafe { &open62541_sys::UA_VariableAttributes_default })
     }
 }
