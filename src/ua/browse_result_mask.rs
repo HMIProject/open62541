89bf5c13092115a3d2cadc5e7b6c4cd6a97c875b
Sebastian Goll <goll@hmi-project.com>
2024-04-29 16:25:15 +0200

    Add tests for bitmask combinators

--- src/ua/browse_result_mask.rs
+++ src/ua/browse_result_mask.rs
@@ -108,3 +108,22 @@ impl BrowseResultMask {
         self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ua;
+
+    #[test]
+    fn combine_masks() {
+        // We support mask combinators with `|` shorthand notation.
+        let lhs = ua::BrowseResultMask::REFERENCETYPEID | ua::BrowseResultMask::ISFORWARD;
+        let rhs = ua::BrowseResultMask::REFERENCETYPEINFO;
+        assert_eq!(lhs, rhs);
+
+        // We support mask combinators in `const` expressions.
+        const LHS: ua::BrowseResultMask =
+            ua::BrowseResultMask::REFERENCETYPEID.or(&ua::BrowseResultMask::ISFORWARD);
+        const RHS: ua::BrowseResultMask = ua::BrowseResultMask::REFERENCETYPEINFO;
+        assert_eq!(LHS, RHS);
+    }
+}
