a182495b375f3b743e53cb5e61b59b4717232ce9
Sebastian Goll <1277035+sgoll@users.noreply.github.com>
2024-05-08 12:50:19 +0200

    Add example that triggers multi-threaded assertion (#104)
    ## Description
    
    This PR is a follow-up to #102 and adds an example that triggers the
    assertion error in https://github.com/open62541/open62541/issues/6455.
    
    A typical run looks like this:
    
    ```
    > cargo run --example async_concurrent
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.07s
         Running `target/debug/examples/async_concurrent`
    Children: 26
    Children: 26
    Children: 26
    Assertion failed: (lock->mutexCounter == num), function UA_LOCK_ASSERT, file config.h, line 422.
    fish: Job 1, 'cargo run --example async_read_…' terminated by signal SIGABRT (Abort)
    ```
    
    ---------
    
    Co-authored-by: Uwe Klotz <uwe.klotz@gmail.com>

---
+++ examples/async_concurrent.rs
@@ -0,0 +1,45 @@
+use std::sync::Arc;
+
+use anyhow::{anyhow, Context as _};
+use open62541::{ua, AsyncClient};
+use open62541_sys::{UA_NS0ID_SERVER, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME};
+use tokio::task::JoinSet;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let client =
+        Arc::new(AsyncClient::new("opc.tcp://opcuademo.sterfive.com:26543").context("connect")?);
+
+    let mut tasks = JoinSet::new();
+
+    // Spawn many tasks in order to trigger race conditions in multi-threaded use of open62541.
+    //
+    for _ in 0..100 {
+        let client = Arc::clone(&client);
+
+        tasks.spawn(async move {
+            let (references, _) = client
+                .browse(&ua::NodeId::ns0(UA_NS0ID_SERVER))
+                .await
+                .context("browse")?;
+            println!("References: {}", references.len());
+
+            let value = client
+                .read_value(&ua::NodeId::ns0(UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME))
+                .await
+                .context("read")?;
+            let value = value.value().ok_or(anyhow!("no value"))?.to_value();
+            println!("Value: {value:?}");
+
+            Ok::<_, anyhow::Error>(())
+        });
+    }
+
+    while let Some(result) = tasks.join_next().await {
+        result??;
+    }
+
+    Ok(())
+}
