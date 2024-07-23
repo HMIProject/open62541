dc48cd28a29a40d9f80860feef789616da4a94f6
Sebastian Goll <goll@hmi-project.com>
2024-07-23 13:04:57 +0200

    Reorder methods to match attributes in C library

--- src/ua/data_types/node_attributes/variable_attributes.rs
+++ src/ua/data_types/node_attributes/variable_attributes.rs
@@ -8,14 +8,14 @@ impl super::VariableAttributes {
     }
 
     #[must_use]
-    pub fn with_access_level(mut self, access_level: &ua::AccessLevel) -> Self {
-        self.0.accessLevel = access_level.as_u8();
+    pub fn with_value_rank(mut self, rank: i32) -> Self {
+        self.0.valueRank = rank;
         self
     }
 
     #[must_use]
-    pub fn with_value_rank(mut self, rank: i32) -> Self {
-        self.0.valueRank = rank;
+    pub fn with_access_level(mut self, access_level: &ua::AccessLevel) -> Self {
+        self.0.accessLevel = access_level.as_u8();
         self
     }
 }
