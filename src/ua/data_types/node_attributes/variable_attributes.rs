dfeb57d6655baedecf40b5abf190150b9bab35c6
Sebastian Goll <goll@hmi-project.com>
2024-07-23 16:04:39 +0200

    Add missing update of `specified_attributes` value after mutation

--- src/ua/data_types/node_attributes/variable_attributes.rs
+++ src/ua/data_types/node_attributes/variable_attributes.rs
@@ -4,18 +4,21 @@ impl super::VariableAttributes {
     #[must_use]
     pub fn with_data_type(mut self, data_type: &ua::NodeId) -> Self {
         data_type.clone_into_raw(&mut self.0.dataType);
+        self.0.specifiedAttributes |= ua::SpecifiedAttributes::DATATYPE.as_u32();
         self
     }
 
     #[must_use]
     pub fn with_value_rank(mut self, rank: i32) -> Self {
         self.0.valueRank = rank;
+        self.0.specifiedAttributes |= ua::SpecifiedAttributes::VALUERANK.as_u32();
         self
     }
 
     #[must_use]
     pub fn with_access_level(mut self, access_level: &ua::AccessLevel) -> Self {
         self.0.accessLevel = access_level.as_u8();
+        self.0.specifiedAttributes |= ua::SpecifiedAttributes::ACCESSLEVEL.as_u32();
         self
     }
 }
