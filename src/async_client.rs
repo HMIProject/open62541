951048e273d0a1f93ccd28f6f5dab5b1f3379aee
Sebastian Goll <goll@hmi-project.com>
2024-07-31 17:24:39 +0200

    Do not request timestamps when reading attribute in client

--- src/async_client.rs
+++ src/async_client.rs
@@ -248,7 +248,10 @@ impl AsyncClient {
             })
             .collect();
 
-        let request = ua::ReadRequest::init().with_nodes_to_read(&nodes_to_read);
+        let request = ua::ReadRequest::init()
+            // We do return and therefore do not request timestamps now.
+            .with_timestamps_to_return(ua::TimestampsToReturn::NEITHER)
+            .with_nodes_to_read(&nodes_to_read);
 
         let response = service_request(&self.client, request).await?;
 
