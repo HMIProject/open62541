20e3d4686842159f27020a7b16a449cb2cade064
Uwe Klotz <uwe.klotz@slowtec.de>
2024-11-20 00:20:07 +0100

    Use synchronous loop for running and FnMut() for cancelling server

--- src/server.rs
+++ src/server.rs
@@ -1296,7 +1296,7 @@ impl Server {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ServerRunner(Arc<ua::Server>);
 
 impl ServerRunner {
@@ -1321,7 +1321,7 @@ impl ServerRunner {
         Error::verify_good(&status_code)
     }
 
-    /// Runs the server until it's cancelled.
+    /// Runs the server until it is cancelled.
     ///
     /// The server is shut down cleanly upon receiving a cancellation token (could be due `SIGINT` or
     /// `SIGTERM`) at which point the method returns.
@@ -1329,12 +1329,7 @@ impl ServerRunner {
     /// # Errors
     ///
     /// This fails when the server cannot be started.
-    #[cfg(feature = "tokio")]
-    pub async fn run_with_cancellation_token(
-        self,
-        task_tracker: tokio_util::task::TaskTracker,
-        cancellation_token: tokio_util::sync::CancellationToken,
-    ) -> Result<()> {
+    pub fn run_until_cancelled(self, is_cancelled: &mut impl FnMut() -> bool) -> Result<()> {
         let status_code = ua::StatusCode::new(unsafe {
             // The prologue part of UA_Server_run.
             open62541_sys::UA_Server_run_startup(
@@ -1347,50 +1342,32 @@ impl ServerRunner {
         });
         Error::verify_good(&status_code)?;
 
-        let cancellation_token_clone = cancellation_token.clone();
-        let jh_run_iterate = task_tracker.spawn_blocking(move || {
-            while !cancellation_token.is_cancelled() {
-                unsafe {
-                    // Executes a single iteration of the server's main loop.
-                    let _ = open62541_sys::UA_Server_run_iterate(
-                        // SAFETY: Cast to `mut` pointer. Function is not marked `UA_THREADSAFE` but we make
-                        // sure that it can only be invoked a single time (ownership of `ServerRunner`). The
-                        // examples in `open62541` demonstrate that running the server in its own thread and
-                        // interacting with it as we do through `Server` is okay.
-                        self.0.as_ptr().cast_mut(),
-                        true,
-                    );
-                }
-            }
-
-            let status_code = ua::StatusCode::new(unsafe {
-                // The epilogue part of UA_Server_run.
-                open62541_sys::UA_Server_run_shutdown(
+        while !is_cancelled() {
+            unsafe {
+                // Executes a single iteration of the server's main loop.
+                let _ = open62541_sys::UA_Server_run_iterate(
                     // SAFETY: Cast to `mut` pointer. Function is not marked `UA_THREADSAFE` but we make
                     // sure that it can only be invoked a single time (ownership of `ServerRunner`). The
                     // examples in `open62541` demonstrate that running the server in its own thread and
                     // interacting with it as we do through `Server` is okay.
                     self.0.as_ptr().cast_mut(),
-                )
-            });
-            log::debug!("UA_Server_run_shutdown status {} ", status_code);
-            Error::verify_good(&status_code)
+                    true,
+                );
+            }
+        }
+
+        let status_code = ua::StatusCode::new(unsafe {
+            // The epilogue part of UA_Server_run.
+            open62541_sys::UA_Server_run_shutdown(
+                // SAFETY: Cast to `mut` pointer. Function is not marked `UA_THREADSAFE` but we make
+                // sure that it can only be invoked a single time (ownership of `ServerRunner`). The
+                // examples in `open62541` demonstrate that running the server in its own thread and
+                // interacting with it as we do through `Server` is okay.
+                self.0.as_ptr().cast_mut(),
+            )
         });
-
-        let result = tokio::select! {
-            () = cancellation_token_clone.cancelled() => {
-                // NOTE: We don't call `UA_Server_run_shutdown` in this flow as `ServerRunner` is not cloneable.
-                Ok(())
-            }
-            o = jh_run_iterate => {
-                match o {
-                    Ok(Ok(())) => Ok(()),
-                    _ => Err(Error::new(ua::StatusCode::BADUNEXPECTEDERROR))
-                }
-            }
-        };
-
-        result
+        log::debug!("UA_Server_run_shutdown status {} ", status_code);
+        Error::verify_good(&status_code)
     }
 }
 
