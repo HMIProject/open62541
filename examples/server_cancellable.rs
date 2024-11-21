0752c39c17b63b90aee63d064d789b0bb6a3b4f5
Sebastian Goll <goll@hmi-project.com>
2024-11-21 15:12:32 +0100

    Clean up example code somewhat

--- examples/server_cancellable.rs
+++ examples/server_cancellable.rs
@@ -6,21 +6,22 @@ use std::{
     time::Duration,
 };
 
-use open62541::ServerBuilder;
+use open62541::Server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    println!("Building server");
-    let (_, runner) = ServerBuilder::default().port(4841).build();
+    let (_, runner) = Server::new();
 
     println!("Starting server");
+
     let cancel_runner = Arc::new(AtomicBool::new(false));
     let mut is_runner_cancelled = {
         let cancel_runner = Arc::clone(&cancel_runner);
         move || cancel_runner.load(Ordering::Relaxed)
     };
+
     let runner_handle = tokio::task::spawn_blocking(move || {
         println!("Server started");
         let result = runner.run_until_cancelled(&mut is_runner_cancelled);
@@ -30,7 +31,8 @@ async fn main() -> anyhow::Result<()> {
 
     tokio::time::sleep(Duration::from_secs(15)).await;
 
-    println!("Stopping server");
+    println!("Cancelling server");
+
     cancel_runner.store(true, Ordering::Relaxed);
     runner_handle.await.unwrap().unwrap();
 
