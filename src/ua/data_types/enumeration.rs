a8aeabb6b886db01232e8556e78c30d99c893922
Sebastian Goll <goll@hmi-project.com>
2025-03-19 13:06:29 +0100

    Fix linter warnings

--- src/ua/data_types/enumeration.rs
+++ src/ua/data_types/enumeration.rs
@@ -2,6 +2,8 @@ crate::data_type!(Enumeration);
 
 impl Enumeration {
     /// Extracts raw enum value.
+    #[must_use]
+    #[allow(clippy::missing_panics_doc)] // Technicality, independent of user data.
     pub fn as_u32(&self) -> u32 {
         // This cast is necessary on Windows builds with inner type `i32`.
         #[allow(clippy::useless_conversion)]
