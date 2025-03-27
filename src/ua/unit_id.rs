94db1e6943212a3f8fb1ac0226f378ddaa3b58f0
Uwe Klotz <uwe.klotz@slowtec.de>
2025-03-27 16:03:08 +0100

    Restore tests

--- src/ua/unit_id.rs
+++ src/ua/unit_id.rs
@@ -35,3 +35,18 @@ impl UnitId {
         .ok()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ua::UnitId;
+
+    #[test]
+    fn unece_code_from_unit_id() {
+        assert_eq!(UnitId::new(12592).to_unece_code().unwrap(), "10"); // group
+        assert_eq!(UnitId::new(12851).to_unece_code().unwrap(), "23"); // gram per cubic centimetre
+        assert_eq!(UnitId::new(17476).to_unece_code().unwrap(), "DD"); // degree [unit of angle]
+        assert_eq!(UnitId::new(23130).to_unece_code().unwrap(), "ZZ"); // mutually defined
+        assert_eq!(UnitId::new(4405297).to_unece_code().unwrap(), "C81"); // radian
+        assert_eq!(UnitId::new(5910833).to_unece_code().unwrap(), "Z11"); // hanging container
+    }
+}
