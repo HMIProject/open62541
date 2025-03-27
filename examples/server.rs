0d02dc8bb7ae9d193f7852468d671fe0ecd59240
Sebastian Goll <goll@hmi-project.com>
2025-03-27 09:24:27 +0100

    Extract background tasks into functions in example

--- examples/server.rs
+++ examples/server.rs
@@ -7,7 +7,7 @@ use std::{
     time::Duration,
 };
 
-use open62541::{ua, Attribute, ObjectNode, Server, VariableNode};
+use open62541::{ua, Attribute, ObjectNode, Server, ServerRunner, VariableNode};
 use open62541_sys::{
     UA_NS0ID_BASEDATAVARIABLETYPE, UA_NS0ID_FOLDERTYPE, UA_NS0ID_OBJECTSFOLDER, UA_NS0ID_ORGANIZES,
     UA_NS0ID_STRING,
@@ -85,47 +85,17 @@ fn main() -> anyhow::Result<()> {
     let statistics_task_handle = thread::spawn({
         let server = server.clone();
         let cancelled = Arc::clone(&cancelled);
-
-        move || -> anyhow::Result<()> {
-            while !cancelled.load(Ordering::Relaxed) {
-                // SAFETY: This is not actually safe, but there is no other way to get the server
-                // statistics at the moment.
-                let statistics = unsafe { server.statistics() };
-                println!("{statistics:?}");
-                thread::sleep(Duration::from_millis(1000));
-            }
-
-            Ok(())
-        }
+        move || statistics_task(&server, &cancelled)
     });
 
     // Start background task that simulates changing variable values.
     let server_task_handle = thread::spawn({
         let cancelled = Arc::clone(&cancelled);
-
-        move || -> anyhow::Result<()> {
-            println!("Simulating values");
-            loop {
-                for value in ["foo", "bar", "baz"] {
-                    if cancelled.load(Ordering::Relaxed) {
-                        return Ok(());
-                    }
-                    server.write_value(
-                        &value_node_id,
-                        &ua::Variant::scalar(ua::String::new(value)?),
-                    )?;
-                    thread::sleep(Duration::from_millis(1000));
-                }
-            }
-        }
+        move || server_task(&server, &cancelled, &value_node_id)
     });
 
     // Start runner task that handles incoming connections (events).
-    let runner_task_handle = thread::spawn(|| -> anyhow::Result<()> {
-        println!("Running server");
-        runner.run()?;
-        Ok(())
-    });
+    let runner_task_handle = thread::spawn(|| runner_task(runner));
 
     // Wait for runner task to finish eventually (SIGINT/Ctrl+C).
     if let Err(err) = runner_task_handle
@@ -145,12 +115,9 @@ fn main() -> anyhow::Result<()> {
         println!("Server task failed: {err}");
     }
     // Wait for statistics task to shut down after cancelling.
-    if let Err(err) = statistics_task_handle
+    statistics_task_handle
         .join()
-        .expect("server task should not panic")
-    {
-        println!("Server task failed: {err}");
-    }
+        .expect("server task should not panic");
 
     println!("Done");
 
@@ -165,6 +132,44 @@ fn read_attribute(
     let value = server.read_attribute(node_id, attribute)?;
 
     println!("- attribute {attribute:?} has value {value:?}");
+
+    Ok(())
+}
+
+fn statistics_task(server: &Server, cancelled: &AtomicBool) {
+    while !cancelled.load(Ordering::Relaxed) {
+        // SAFETY: This is not actually safe, but there is no other way to get the server
+        // statistics at the moment.
+        let statistics = unsafe { server.statistics() };
+        println!("{statistics:?}");
+        thread::sleep(Duration::from_millis(1000));
+    }
+}
+
+fn server_task(
+    server: &Server,
+    cancelled: &AtomicBool,
+    value_node_id: &ua::NodeId,
+) -> anyhow::Result<()> {
+    println!("Simulating values");
+
+    loop {
+        for value in ["foo", "bar", "baz"] {
+            if cancelled.load(Ordering::Relaxed) {
+                return Ok(());
+            }
+
+            server.write_value(value_node_id, &ua::Variant::scalar(ua::String::new(value)?))?;
+
+            thread::sleep(Duration::from_millis(1000));
+        }
+    }
+}
+
+fn runner_task(runner: ServerRunner) -> anyhow::Result<()> {
+    println!("Running server");
+
+    runner.run()?;
 
     Ok(())
 }
