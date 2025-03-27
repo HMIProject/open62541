d0097a46d44a26d53cdc1ca8a94748248a70d073
Uwe Klotz <uwe.klotz@slowtec.de>
2025-03-27 15:59:46 +0100

    cleanup

--- src/ua/data_types/eu_information.rs
+++ src/ua/data_types/eu_information.rs
@@ -2,17 +2,14 @@
 //!
 //! See also: <https://reference.opcfoundation.org/Core/Part8/v104/docs/5.6.3>
 
-use crate::{
-    ua::{self, UnitId},
-    DataType as _,
-};
+use crate::{ua, DataType as _};
 
 crate::data_type!(EUInformation);
 
 impl EUInformation {
     #[must_use]
     pub const fn unit_id(&self) -> ua::UnitId {
-        UnitId::new(self.0.unitId)
+        ua::UnitId::new(self.0.unitId)
     }
 
     #[must_use]
