ea70cfc7c6829b55d05f0e614a8aa6089c226aa1
Uwe Klotz <uwe.klotz@gmail.com>
2025-03-29 19:45:33 +0100

    More strict validation in UnitId::to_unece_code() (#211)
    Co-authored-by: Sebastian Goll <1277035+sgoll@users.noreply.github.com>

--- src/ua/unit_id.rs
+++ src/ua/unit_id.rs
@@ -13,26 +13,39 @@ impl UnitId {
     /// Decoded from the wrapped integer value according to the
     /// [mapping defined by OPC UA](https://reference.opcfoundation.org/Core/Part8/v104/docs/5.6.3).
     ///
-    /// The returned string is not guaranteed to represent a valid code.
-    /// Returns `None` if the value could not be decoded.
+    /// Returns an ASCII alphanumeric string with a minimum length of 1 and
+    /// a maximum length of 3. All other strings are discarded as invalid.
+    /// `unit_id`s where the most significant byte is not `0x00` are rejected
+    /// as invalid.
     ///
-    /// See also: <http://www.opcfoundation.org/UA/EngineeringUnits/UNECE/UNECE_to_OPCUA.csv>
+    /// Returns `None` if the value could not be decoded or is invalid.
+    ///
+    /// The returned string is not guaranteed to represent a valid _Common Code_
+    /// as defined by the [UNECE documents] or the [OPC UA specification].
+    ///
+    /// [UNECE documents]: <https://unece.org/trade/documents/session-documents/revision-6>
+    /// [OPC UA specification]: <http://www.opcfoundation.org/UA/EngineeringUnits/UNECE/UNECE_to_OPCUA.csv>
     #[must_use]
-    #[allow(clippy::missing_panics_doc)] // Bitmasks restrict values.
     pub fn to_unece_code(&self) -> Option<String> {
         let Self(unit_id) = self;
-        // TODO: More strict validation would require to inspect the official spec.
-        String::from_utf8(
-            [
-                u8::try_from((unit_id & 0x00ff_0000) >> 16).expect("always in range"),
-                u8::try_from((unit_id & 0x0000_ff00) >> 8).expect("always in range"),
-                u8::try_from(unit_id & 0x0000_00ff).expect("always in range"),
-            ]
-            .into_iter()
+        let ascii_chars = unit_id
+            .to_be_bytes()
+            .iter()
+            .copied()
             .skip_while(|c| *c == 0x00)
-            .collect(),
-        )
-        .ok()
+            .map(|c| {
+                // TODO: Are lowercase ASCII characters allowed? Probably not.
+                c.is_ascii_alphanumeric().then_some(c)
+            })
+            .collect::<Option<Vec<_>>>()?;
+        // TODO: Restrict minimum length to 2?
+        #[allow(clippy::len_zero)] // Symmetric bounds checks.
+        if !(1..=3).contains(&ascii_chars.len()) {
+            return None;
+        }
+        let code = String::from_utf8(ascii_chars);
+        debug_assert!(code.is_ok(), "never fails for ASCII character codes");
+        code.ok()
     }
 }
 
@@ -48,5 +61,24 @@ mod tests {
         assert_eq!(UnitId::new(23_130).to_unece_code().unwrap(), "ZZ"); // mutually defined
         assert_eq!(UnitId::new(4_405_297).to_unece_code().unwrap(), "C81"); // radian
         assert_eq!(UnitId::new(5_910_833).to_unece_code().unwrap(), "Z11"); // hanging container
+
+        // Allow only codes with expected length.
+        assert!(UnitId::new(0x0000_0000).to_unece_code().is_none());
+        assert!(UnitId::new(0x3000_0000).to_unece_code().is_none());
+        assert!(
+            UnitId::new(0x0000_0030).to_unece_code().is_some(),
+            "valid code of 1 ASCII character"
+        );
+        assert!(UnitId::new(0x3000_0030).to_unece_code().is_none());
+        assert!(
+            UnitId::new(0x0000_3030).to_unece_code().is_some(),
+            "valid code of 2 ASCII characters"
+        );
+        assert!(UnitId::new(0x3000_3030).to_unece_code().is_none());
+        assert!(
+            UnitId::new(0x0030_3030).to_unece_code().is_some(),
+            "valid code of 3 ASCII characters"
+        );
+        assert!(UnitId::new(0x3030_3030).to_unece_code().is_none());
     }
 }
