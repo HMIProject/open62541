65c4df365d492ca1c02c39d1ff7778a49a4b88f8
Uwe Klotz <uwe.klotz@slowtec.de>
2025-03-31 14:54:45 +0200

    Add field accessors and comments

--- src/ua/data_types/guid.rs
+++ src/ua/data_types/guid.rs
@@ -2,6 +2,30 @@ crate::data_type!(Guid);
 
 impl Guid {
     #[must_use]
+    pub const fn data1(&self) -> u32 {
+        self.0.data1
+    }
+
+    #[must_use]
+    pub const fn data2(&self) -> u16 {
+        self.0.data2
+    }
+
+    #[must_use]
+    pub const fn data3(&self) -> u16 {
+        self.0.data3
+    }
+
+    #[must_use]
+    pub const fn data4(&self) -> [u8; 8] {
+        self.0.data4
+    }
+}
+
+#[cfg(feature = "uuid")]
+impl Guid {
+    /// Creates a [`Guid`] from a [`Uuid`](uuid::Uuid).
+    #[must_use]
     #[cfg(feature = "uuid")]
     pub fn from_uuid(uuid: uuid::Uuid) -> Self {
         use crate::DataType as _;
@@ -14,8 +38,10 @@ impl Guid {
         guid
     }
 
+    /// Converts the [`Guid`] into a [`Uuid`](uuid::Uuid).
+    ///
+    /// The bitwise conversion might not result in a valid v1/v2/v3/v4/v5/v6/v7/v8 UUID.
     #[must_use]
-    #[cfg(feature = "uuid")]
     pub const fn to_uuid(&self) -> uuid::Uuid {
         uuid::Uuid::from_fields(self.0.data1, self.0.data2, self.0.data3, &self.0.data4)
     }
