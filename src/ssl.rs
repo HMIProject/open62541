4cf38e31bfa182d545920c5752e131bddeb0d8a4
Sebastian Goll <1277035+sgoll@users.noreply.github.com>
2025-04-08 14:04:59 +0200

    Silence false-positive clippy warnings suggesting `const fn` (#216)
    This allows some instances of `clippy::missing_const_for_fn` where that
    lint is reporting that a function could be `const` when it really can't
    (adding `const` causes a compile error).

--- src/ssl.rs
+++ src/ssl.rs
@@ -39,6 +39,7 @@ impl Password {
         unsafe { self.0.as_bytes_unchecked() }
     }
 
+    #[allow(clippy::missing_const_for_fn)] // false positive
     pub(crate) fn as_byte_string(&self) -> &ua::ByteString {
         &self.0
     }
@@ -160,6 +161,7 @@ impl PrivateKey {
         unsafe { self.0.as_bytes_unchecked() }
     }
 
+    #[allow(clippy::missing_const_for_fn)] // false positive
     pub(crate) fn as_byte_string(&self) -> &ua::ByteString {
         &self.0
     }
