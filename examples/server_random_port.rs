a2aadc27bf9aab64d6fb2509a1f245d2a44d2223
Sebastian Goll <goll@hmi-project.com>
2025-05-31 01:04:57 +0200

    Use if-let instead of match statement in example

--- examples/server_random_port.rs
+++ examples/server_random_port.rs
@@ -9,26 +9,23 @@ fn main() {
 
     let handle = std::thread::spawn(|| runner.run_until_cancelled(&mut || true).unwrap());
 
-    match server.discovery_urls() {
-        None => {
-            println!("No discovery URLs");
-        }
-        Some(urls) => {
-            println!("Discovery URLs: {urls:?}");
+    if let Some(urls) = server.discovery_urls() {
+        println!("Discovery URLs: {urls:?}");
 
-            if let Some(tail) = urls
-                .as_slice()
-                .first()
-                .and_then(|url| url.as_str())
-                .and_then(|url| url.strip_prefix("opc.tcp://127.0.0.1:"))
-            {
-                if let Ok(port) = tail.parse::<u16>() {
-                    println!("Server running on port {port}");
-                } else {
-                    println!("Unable to find port");
-                }
+        if let Some(tail) = urls
+            .as_slice()
+            .first()
+            .and_then(|url| url.as_str())
+            .and_then(|url| url.strip_prefix("opc.tcp://127.0.0.1:"))
+        {
+            if let Ok(port) = tail.parse::<u16>() {
+                println!("Server running on port {port}");
+            } else {
+                println!("Unable to find port");
             }
         }
+    } else {
+        println!("No discovery URLs");
     }
 
     handle.join().unwrap();
