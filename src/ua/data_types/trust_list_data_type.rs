531393826fd9cc45e376f80bfe8a185f5670a562
Sebastian Goll <goll@hmi-project.com>
2025-09-01 15:58:05 +0200

    Set trust list masks

--- src/ua/data_types/trust_list_data_type.rs
+++ src/ua/data_types/trust_list_data_type.rs
@@ -6,39 +6,54 @@ impl TrustListDataType {
     #[must_use]
     pub fn with_trusted_certificates(mut self, trusted_certificates: &[ua::String]) -> Self {
         let array = ua::Array::from_slice(trusted_certificates);
+        if array.is_empty() {
+            self.0.specifiedLists &= !ua::TrustListMasks::TRUSTEDCERTIFICATES.as_u32();
+        } else {
+            self.0.specifiedLists |= ua::TrustListMasks::TRUSTEDCERTIFICATES.as_u32();
+        }
         array.move_into_raw(
             &mut self.0.trustedCertificatesSize,
             &mut self.0.trustedCertificates,
         );
-        // FIXME: How to set masks?
-        // self.0.specifiedLists |= UA_TRUSTLISTMASKS_TRUSTEDCERTIFICATES;
         self
     }
 
     #[must_use]
     pub fn with_trusted_crls(mut self, trusted_crls: &[ua::String]) -> Self {
         let array = ua::Array::from_slice(trusted_crls);
+        if array.is_empty() {
+            self.0.specifiedLists &= !ua::TrustListMasks::TRUSTEDCRLS.as_u32();
+        } else {
+            self.0.specifiedLists |= ua::TrustListMasks::TRUSTEDCRLS.as_u32();
+        }
         array.move_into_raw(&mut self.0.trustedCrlsSize, &mut self.0.trustedCrls);
-        // FIXME: How to set masks?
         self
     }
 
     #[must_use]
     pub fn with_issuer_certificates(mut self, issuer_certificates: &[ua::String]) -> Self {
         let array = ua::Array::from_slice(issuer_certificates);
+        if array.is_empty() {
+            self.0.specifiedLists &= !ua::TrustListMasks::ISSUERCERTIFICATES.as_u32();
+        } else {
+            self.0.specifiedLists |= ua::TrustListMasks::ISSUERCERTIFICATES.as_u32();
+        }
         array.move_into_raw(
             &mut self.0.issuerCertificatesSize,
             &mut self.0.issuerCertificates,
         );
-        // FIXME: How to set masks?
         self
     }
 
     #[must_use]
     pub fn with_issuer_crls(mut self, issuer_crls: &[ua::String]) -> Self {
         let array = ua::Array::from_slice(issuer_crls);
+        if array.is_empty() {
+            self.0.specifiedLists &= !ua::TrustListMasks::ISSUERCRLS.as_u32();
+        } else {
+            self.0.specifiedLists |= ua::TrustListMasks::ISSUERCRLS.as_u32();
+        }
         array.move_into_raw(&mut self.0.issuerCrlsSize, &mut self.0.issuerCrls);
-        // FIXME: How to set masks?
         self
     }
 }
