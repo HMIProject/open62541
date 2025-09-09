760daf5d5e6b9310b68ea5c45f83cde02426a366
Uwe Klotz <klotz@hmi-project.com>
2025-09-09 20:01:40 +0200

    Extract common code for monitored items
    Split off separate submodule `monitored_items` that is independent of
    the optional feature `tokio`.

--- src/async_monitored_item.rs
+++ src/async_monitored_item.rs
@@ -1,6 +1,3 @@
-mod create_monitored_items;
-mod delete_monitored_items;
-
 use std::{
     marker::PhantomData,
     pin::Pin,
@@ -13,9 +10,13 @@ use futures_core::Stream;
 use tokio::sync::mpsc;
 
 use crate::{
-    attributes, ua, AsyncSubscription, Attribute, DataType as _, DataValue, Error,
-    MonitoringFilter, Result,
+    attributes,
+    monitored_item::{DataChange, Unknown},
+    ua, AsyncSubscription, DataType as _, Error, MonitoredItemAttribute, MonitoredItemKind,
+    MonitoredItemValue, MonitoringFilter, Result,
 };
+
+mod create_monitored_items;
 
 #[derive(Debug)]
 pub struct MonitoredItemBuilder<K: MonitoredItemKind> {
@@ -50,7 +51,7 @@ impl MonitoredItemBuilder<DataChange<attributes::Value>> {
 impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
     /// Sets attribute.
     ///
-    /// By default, monitored items emit [`DataValue`] of the appropriate subtype matching the given
+    /// By default, monitored items emit [`DataValue`](crate::DataValue) of the appropriate subtype matching the given
     /// attribute. If the attribute is set to [`ua::AttributeId::EVENTNOTIFIER_T`], they emit
     /// `ua::Array<ua::Variant>` instead.
     ///
@@ -262,7 +263,7 @@ impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
                 .with_subscription_id(subscription_id)
                 .with_monitored_item_ids(&monitored_item_ids);
             // This request is processed asynchronously. Errors are logged asynchronously too.
-            delete_monitored_items::call(client, &request);
+            crate::delete_monitored_items::call(client, &request);
 
             return Err(Error::internal("unexpected number of monitored items"));
         }
@@ -332,67 +333,6 @@ impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
     }
 }
 
-/// Value emitted from monitored item notification.
-///
-/// This depends on the attribute ID passed to [`MonitoredItemBuilder::attribute_id()`].
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct MonitoredItemValue(MonitoredItemValueInner);
-
-impl MonitoredItemValue {
-    #[must_use]
-    const fn data_change(value: ua::DataValue) -> Self {
-        Self(MonitoredItemValueInner::DataChange { value })
-    }
-
-    #[must_use]
-    const fn event(fields: ua::Array<ua::Variant>) -> Self {
-        Self(MonitoredItemValueInner::Event { fields })
-    }
-
-    /// Gets data change payload.
-    ///
-    /// This returns `None` for event monitored items.
-    #[must_use]
-    pub const fn value(&self) -> Option<&ua::DataValue> {
-        match &self.0 {
-            MonitoredItemValueInner::DataChange { value } => Some(value),
-            MonitoredItemValueInner::Event { fields: _ } => None,
-        }
-    }
-
-    /// Gets event payload.
-    ///
-    /// This returns `None` for data change monitored items.
-    #[must_use]
-    pub const fn fields(&self) -> Option<&[ua::Variant]> {
-        match &self.0 {
-            MonitoredItemValueInner::DataChange { value: _ } => None,
-            MonitoredItemValueInner::Event { fields } => Some(fields.as_slice()),
-        }
-    }
-
-    #[must_use]
-    fn into_inner(self) -> MonitoredItemValueInner {
-        self.0
-    }
-}
-
-// We consider both variants as distinct by deriving `Eq`.
-// But there is no canonical ordering between those variants
-// for implementing `Ord`.
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum MonitoredItemValueInner {
-    /// Data change payload.
-    ///
-    /// This is emitted for attribute IDs other than [`ua::AttributeId::EVENTNOTIFIER`].
-    DataChange { value: ua::DataValue },
-
-    /// Event payload.
-    ///
-    /// This is emitted for attribute ID [`ua::AttributeId::EVENTNOTIFIER`].
-    Event { fields: ua::Array<ua::Variant> },
-}
-
 /// Monitored item (with asynchronous API).
 #[derive(Debug)]
 pub struct AsyncMonitoredItem<K: MonitoredItemKind = DataChange<attributes::Value>> {
@@ -448,7 +388,7 @@ impl<K: MonitoredItemKind> Drop for AsyncMonitoredItem<K> {
             .with_subscription_id(self.subscription_id)
             .with_monitored_item_ids(&[self.monitored_item_id]);
 
-        delete_monitored_items::call(&client, &request);
+        crate::delete_monitored_items::call(&client, &request);
     }
 }
 
@@ -465,135 +405,3 @@ impl<K: MonitoredItemKind> Stream for AsyncMonitoredItem<K> {
 }
 
 impl<K: MonitoredItemKind> Unpin for AsyncMonitoredItem<K> {}
-
-/// Sealed typestate used in [`MonitoredItemBuilder`].
-pub trait MonitoredItemKind: sealed::MonitoredItemKind + Send + Sync + 'static {
-    type Value;
-
-    fn map_value(value: MonitoredItemValue) -> Self::Value;
-}
-
-/// Typestate for [`MonitoredItemKind`] that yields data change notifications.
-#[derive(Debug)]
-pub struct DataChange<T: Attribute>(PhantomData<T>);
-
-impl<T: DataChangeAttribute + Send + Sync + 'static> MonitoredItemKind for DataChange<T> {
-    type Value = DataValue<T::Value>;
-
-    fn map_value(value: MonitoredItemValue) -> Self::Value {
-        match value.into_inner() {
-            MonitoredItemValueInner::DataChange { value } => value.cast(),
-            MonitoredItemValueInner::Event { fields: _ } => {
-                // PANIC: Typestate uses attribute ID to enforce callback method.
-                unreachable!("unexpected event payload in data change notification");
-            }
-        }
-    }
-}
-
-/// Typestate for [`MonitoredItemKind`] that yields event notifications.
-#[derive(Debug)]
-pub struct Event;
-
-impl MonitoredItemKind for Event {
-    type Value = ua::Array<ua::Variant>;
-
-    fn map_value(value: MonitoredItemValue) -> Self::Value {
-        match value.into_inner() {
-            MonitoredItemValueInner::DataChange { value: _ } => {
-                // PANIC: Typestate uses attribute ID to enforce callback method.
-                unreachable!("unexpected data change payload in event notification");
-            }
-            MonitoredItemValueInner::Event { fields } => fields,
-        }
-    }
-}
-
-/// Typestate for [`MonitoredItemKind`] that yields notifications.
-///
-/// This is used for runtime and/or mixed-type notifications.
-#[derive(Debug)]
-pub struct Unknown;
-
-impl MonitoredItemKind for Unknown {
-    type Value = MonitoredItemValue;
-
-    fn map_value(value: MonitoredItemValue) -> Self::Value {
-        value
-    }
-}
-
-/// Attribute that yields data change notifications.
-///
-/// This is implemented for all attributes except [`ua::AttributeId::EVENTNOTIFIER_T`].
-trait DataChangeAttribute: Attribute {}
-
-/// Attribute for [`MonitoredItemBuilder::attribute()`].
-pub trait MonitoredItemAttribute: Attribute {
-    /// Matching [`MonitoredItemKind`] implementation for attribute.
-    type Kind: MonitoredItemKind;
-}
-
-macro_rules! data_change_impl {
-    ($($name:ident),* $(,)?) => {
-        $(
-            impl DataChangeAttribute for $crate::attributes::$name {}
-
-            impl MonitoredItemAttribute for $crate::attributes::$name {
-                type Kind = DataChange<$crate::attributes::$name>;
-            }
-        )*
-    };
-}
-
-// Note: Array values are not supported yet in their typed form: previously, any such attempt would
-// fail, because converting to `DataValue` expects scalar values.
-//
-// To give us some time to think about the best, typed representation of such non-scalar values, we
-// remove their `impl` for now. Access is still possible with the non-typed attribute methods.
-data_change_impl!(
-    NodeId,
-    NodeClass,
-    BrowseName,
-    DisplayName,
-    Description,
-    WriteMask,
-    UserWriteMask,
-    IsAbstract,
-    Symmetric,
-    InverseName,
-    ContainsNoLoops,
-    // We to _not_ implement `DataChange` kind for `EventNotifier`, because the attribute uses a
-    // dedicated callback function yielding `ua::Array<ua::Variant>` instead of `ua::DataValue`.
-    Value,
-    DataType,
-    ValueRank,
-    // ArrayDimensions,
-    AccessLevel,
-    UserAccessLevel,
-    MinimumSamplingInterval,
-    Historizing,
-    Executable,
-    UserExecutable,
-    DataTypeDefinition,
-    // RolePermissions,
-    // UserRolePermissions,
-    AccessRestrictions,
-    AccessLevelEx,
-);
-
-impl MonitoredItemAttribute for attributes::EventNotifier {
-    type Kind = Event;
-}
-
-mod sealed {
-    use crate::Attribute;
-
-    pub trait MonitoredItemKind {}
-
-    impl<T: Attribute> MonitoredItemKind for super::DataChange<T> {}
-
-    impl MonitoredItemKind for super::Event {}
-
-    impl MonitoredItemKind for super::Unknown {}
-}
