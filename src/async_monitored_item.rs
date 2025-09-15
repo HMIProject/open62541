HMIProject/open62541

commit 4743325d2f67b81812d27192c32adef85a74934a

Use type-safe notification callbacks for monitored items (#276)
Still mapped to `MonitoredItemValue` internally, but that could be resolved later.

src/async_monitored_item.rs
@@ -7,7 +7,7 @@ use crate::{
     attributes, create_monitored_items, delete_monitored_items,
     monitored_item::{DataChange, Unknown},
     ua, AsyncSubscription, DataType as _, Error, MonitoredItemAttribute, MonitoredItemHandle,
-    MonitoredItemKind, MonitoredItemValue, MonitoringFilter, Result,
+    MonitoredItemKind, MonitoringFilter, Result,
 };
 
 /// Maximum number of buffered values.
@@ -107,9 +107,9 @@ impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
 
     /// Sets attribute ID.
     ///
-    /// When using this method, monitored items emit [`MonitoredItemValue`] instead of the specific
-    /// type. See [`Self::attribute()`] for a type-safe alternative that yields appropriately typed
-    /// values for the given monitored attribute directly.
+    /// When using this method, monitored items emit [`MonitoredItemValue`](crate::MonitoredItemValue)
+    /// instead of the specific type. See [`Self::attribute()`] for a type-safe alternative that yields
+    /// appropriately typed values for the given monitored attribute directly.
     ///
     /// Default value is [`ua::AttributeId::VALUE`].
     ///
@@ -247,8 +247,8 @@ impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
                 let (tx, rx) = mpsc::channel(DEFAULT_STREAM_BUFFER_SIZE);
                 rxs.push(rx);
                 debug_assert_eq!(index, rxs.len());
-                move |monitored_item_value| {
-                    if let Err(err) = tx.try_send(monitored_item_value) {
+                move |value| {
+                    if let Err(err) = tx.try_send(value) {
                         match err {
                             TrySendError::Full(_value) => {
                                 // We cannot blockingly wait, because that would block `UA_Client_run_iterate()`
@@ -263,7 +263,7 @@ impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
                     }
                 }
             };
-            create_monitored_items::call(client, &request, create_value_callback_fn).await?
+            create_monitored_items::call::<K, _>(client, &request, create_value_callback_fn).await?
         };
 
         let Some(mut results) = response.into_results() else {
@@ -357,12 +357,12 @@ impl<K: MonitoredItemKind> MonitoredItemBuilder<K> {
 #[derive(Debug)]
 pub struct AsyncMonitoredItem<K: MonitoredItemKind = DataChange<attributes::Value>> {
     handle: MonitoredItemHandle,
-    rx: mpsc::Receiver<MonitoredItemValue>,
+    rx: mpsc::Receiver<K::Value>,
     _kind: PhantomData<K>,
 }
 
 impl<K: MonitoredItemKind> AsyncMonitoredItem<K> {
-    const fn new(handle: MonitoredItemHandle, rx: mpsc::Receiver<MonitoredItemValue>) -> Self {
+    const fn new(handle: MonitoredItemHandle, rx: mpsc::Receiver<K::Value>) -> Self {
         Self {
             handle,
             rx,
@@ -376,7 +376,7 @@ impl<K: MonitoredItemKind> AsyncMonitoredItem<K> {
     /// been closed and no more updates will be received.
     pub async fn next(&mut self) -> Option<K::Value> {
         // This mirrors `<Self as Stream>::poll_next()` but does not require `self` to be pinned.
-        self.rx.recv().await.map(K::map_value)
+        self.rx.recv().await
     }
 
     /// Turns monitored item into stream.
@@ -414,10 +414,7 @@ impl<K: MonitoredItemKind> Stream for AsyncMonitoredItem<K> {
         cx: &mut task::Context<'_>,
     ) -> task::Poll<Option<Self::Item>> {
         // This mirrors `AsyncMonitoredItem::next()` and implements the `Stream` trait.
-        self.as_mut()
-            .rx
-            .poll_recv(cx)
-            .map(|value| value.map(K::map_value))
+        self.as_mut().rx.poll_recv(cx)
     }
 }
 

src/monitored_item/create_monitored_items.rs
@@ -7,18 +7,21 @@ use open62541_sys::{
     UA_DataValue, UA_UInt32, UA_Variant,
 };
 
-use crate::{ua, CallbackMut, CallbackOnce, DataType as _, Error, MonitoredItemValue, Result};
+use crate::{
+    ua, CallbackMut, CallbackOnce, DataType as _, Error, MonitoredItemKind, MonitoredItemValue,
+    Result,
+};
 
 type CbResponse =
     CallbackOnce<std::result::Result<ua::CreateMonitoredItemsResponse, ua::StatusCode>>;
-type CbValue = CallbackMut<MonitoredItemValue>;
+type CbNotification = CallbackMut<MonitoredItemValue>;
 
 // Wrapper type so that we can mark `*mut c_void` for callbacks as safe to send.
 #[repr(transparent)]
 struct Context(*mut c_void);
 
 // SAFETY: As long as payload is `Send`, wrapper is `Send`.
-unsafe impl Send for Context where CbValue: Send + Sync {}
+unsafe impl Send for Context where CbNotification: Send + Sync {}
 
 /// Creates monitored items.
 ///
@@ -32,13 +35,13 @@ unsafe impl Send for Context where CbValue: Send + Sync {}
 // `create_value_callback_fn`? `impl for<'a> FnMut(usize, &'a ua::MonitoredItemCreateRequest) -> F`
 // doesn't work.
 // See also: <https://rust-lang.github.io/rfcs/3216-closure-lifetime-binder.html>
-pub(crate) async fn call<F>(
+pub(crate) async fn call<K: MonitoredItemKind, F>(
     client: &ua::Client,
     request: &ua::CreateMonitoredItemsRequest,
     mut create_value_callback_fn: impl FnMut(usize) -> F,
 ) -> Result<ua::CreateMonitoredItemsResponse>
 where
-    F: FnMut(MonitoredItemValue) + 'static,
+    F: FnMut(K::Value) + 'static,
 {
     let (tx, rx) = oneshot::channel::<Result<ua::CreateMonitoredItemsResponse>>();
 
@@ -53,7 +56,7 @@ where
 
     let mut notification_callbacks: Vec<UA_Client_DataChangeNotificationCallback> =
         Vec::with_capacity(items_to_create.len());
-    let mut delete_callbacks: Vec<UA_Client_DeleteMonitoredItemCallback> =
+    let mut delete_notification_callbacks: Vec<UA_Client_DeleteMonitoredItemCallback> =
         Vec::with_capacity(items_to_create.len());
     let mut contexts = Vec::with_capacity(items_to_create.len());
 
@@ -61,16 +64,22 @@ where
         // `open62541` requires one set of notification/delete callback and context per monitored
         // item in the request.
         let notification_callback = NotificationCallback::new(item_to_create);
-        let delete_callback: UA_Client_DeleteMonitoredItemCallback = Some(delete_callback_c);
+        let delete_notification_callback: UA_Client_DeleteMonitoredItemCallback =
+            Some(delete_notification_callback_c);
+
         // TODO: let value_callback = create_value_callback_fn(item_index, item_to_create);
-        let value_callback = create_value_callback_fn(item_index);
-        let context = Context(CbValue::prepare(value_callback));
+        let mut value_callback = create_value_callback_fn(item_index);
+        let notication_callback = move |value| {
+            // TODO: How to get rid of the intermediate, internal mapping into `MonitoredItemValue`?
+            value_callback(K::map_value(value));
+        };
+        let context = Context(CbNotification::prepare(notication_callback));
 
         // SAFETY: This cast is possible because `UA_Client_MonitoredItems_createDataChanges_async`
         // internally casts the function pointer back to the appropriate type before calling (union
         // type of attribute `handler` in `UA_Client_MonitoredItem`).
         notification_callbacks.push(Some(unsafe { notification_callback.into_data_change() }));
-        delete_callbacks.push(delete_callback);
+        delete_notification_callbacks.push(delete_notification_callback);
         contexts.push(context);
     }
 
@@ -94,7 +103,7 @@ where
                 request,
                 contexts.as_mut_ptr().cast::<*mut c_void>(),
                 notification_callbacks.as_mut_ptr(),
-                delete_callbacks.as_mut_ptr(),
+                delete_notification_callbacks.as_mut_ptr(),
                 Some(response_callback_c),
                 CbResponse::prepare(response_callback),
                 ptr::null_mut(),
@@ -174,9 +183,9 @@ unsafe extern "C" fn data_change_notification_callback_c(
     let value = unsafe { value.as_ref() }.expect("value should be set");
     let value = ua::DataValue::clone_raw(value);
 
-    // SAFETY: `mon_context` is result of `CbValue::prepare()` and is used only before `delete()`.
+    // SAFETY: `mon_context` is result of `CbNotification::prepare()` and is used only before `delete()`.
     unsafe {
-        CbValue::execute(mon_context, MonitoredItemValue::data_change(value));
+        CbNotification::execute(mon_context, MonitoredItemValue::data_change(value));
     }
 }
 
@@ -205,13 +214,13 @@ unsafe extern "C" fn event_notification_callback_c(
     let fields = ua::Array::from_raw_parts(n_event_fields, event_fields)
         .expect("event fields should be set");
 
-    // SAFETY: `mon_context` is result of `CbValue::prepare()` and is used only before `delete()`.
+    // SAFETY: `mon_context` is result of `CbNotification::prepare()` and is used only before `delete()`.
     unsafe {
-        CbValue::execute(mon_context, MonitoredItemValue::event(fields));
+        CbNotification::execute(mon_context, MonitoredItemValue::event(fields));
     }
 }
 
-unsafe extern "C" fn delete_callback_c(
+unsafe extern "C" fn delete_notification_callback_c(
     _client: *mut UA_Client,
     _sub_id: UA_UInt32,
     _sub_context: *mut c_void,
@@ -220,9 +229,9 @@ unsafe extern "C" fn delete_callback_c(
 ) {
     log::debug!("DeleteMonitoredItemCallback() was called");
 
-    // SAFETY: `mon_context` is result of `CbValue::prepare()` and is used only before `delete()`.
+    // SAFETY: `mon_context` is result of `CbNotification::prepare()` and is used only before `delete()`.
     unsafe {
-        CbValue::delete(mon_context);
+        CbNotification::delete(mon_context);
     }
 }
 

src/monitored_item.rs
@@ -182,7 +182,7 @@ enum MonitoredItemValueInner {
 
 /// Sealed typestate trait.
 pub trait MonitoredItemKind: sealed::MonitoredItemKind + Send + Sync + 'static {
-    type Value;
+    type Value: Send;
 
     fn map_value(value: MonitoredItemValue) -> Self::Value;
 }

src/traits.rs
@@ -46,7 +46,7 @@ impl<T: DataType> DataTypeExt for T {
 // FIXME: Turn into sealed trait.
 pub trait Attribute: Debug + Copy {
     /// Attribute data type.
-    type Value: DataTypeExt;
+    type Value: DataTypeExt + Send;
 
     /// Gets attribute ID.
     fn id(&self) -> ua::AttributeId;

commit 30b8312a1ac795ed51bcc34a8089fb3c4ff2f30d

Fix wrong debug assert and confusing typo (#283)
Caused by #276.

src/async_monitored_item.rs
@@ -294,7 +294,6 @@ impl<K: MonitoredItemKind> AsyncMonitoredItem<K> {
                 debug_assert_eq!(index, rxs.len());
                 let (tx, rx) = mpsc::channel(DEFAULT_STREAM_BUFFER_SIZE);
                 rxs.push(rx);
-                debug_assert_eq!(index, rxs.len());
                 move |value| {
                     if let Err(err) = tx.try_send(value) {
                         match err {

src/monitored_item/create_monitored_items.rs
@@ -68,12 +68,12 @@ where
             Some(delete_notification_callback_c);
 
         // TODO: let value_callback = create_value_callback_fn(item_index, item_to_create);
-        let mut value_callback = create_value_callback_fn(item_index);
-        let notication_callback = move |value| {
+        let mut value_callback: F = create_value_callback_fn(item_index);
+        let map_value_callback = move |value| {
             // TODO: How to get rid of the intermediate, internal mapping into `MonitoredItemValue`?
             value_callback(K::map_value(value));
         };
-        let context = Context(CbNotification::prepare(notication_callback));
+        let context = Context(CbNotification::prepare(map_value_callback));
 
         // SAFETY: This cast is possible because `UA_Client_MonitoredItems_createDataChanges_async`
         // internally casts the function pointer back to the appropriate type before calling (union