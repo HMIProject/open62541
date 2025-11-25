394c729589110ce5c300419ba4c91e46c52432c3
Uwe Klotz <klotz@hmi-project.com>
2025-11-25 15:41:19 +0100

    Add `VariableAttributes::with_array_dimensions()` (#304)

--- src/ua/data_types/node_attributes/variable_attributes.rs
+++ src/ua/data_types/node_attributes/variable_attributes.rs
@@ -16,6 +16,16 @@ impl super::VariableAttributes {
     }
 
     #[must_use]
+    pub fn with_array_dimensions(mut self, array_dimensions: &[u32]) -> Self {
+        let array_dimensions =
+            ua::Array::from_iter(array_dimensions.iter().copied().map(ua::UInt32::new));
+        array_dimensions
+            .move_into_raw(&mut self.0.arrayDimensionsSize, &mut self.0.arrayDimensions);
+        self.0.specifiedAttributes |= ua::SpecifiedAttributes::ARRAYDIMENSIONS.as_u32();
+        self
+    }
+
+    #[must_use]
     pub const fn with_access_level(mut self, access_level: &ua::AccessLevelType) -> Self {
         self.0.accessLevel = access_level.as_u8();
         self.0.specifiedAttributes |= ua::SpecifiedAttributes::ACCESSLEVEL.as_u32();
