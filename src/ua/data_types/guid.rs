ca1bd4e08e147884d12ba0a962385196c1145134
Sebastian Goll <goll@hmi-project.com>
2026-01-22 14:13:09 +0100

    Add constructor ua::Guid::new()

--- src/ua/data_types/guid.rs
+++ src/ua/data_types/guid.rs
@@ -1,6 +1,18 @@
+use open62541_sys::UA_Guid;
+
 crate::data_type!(Guid);
 
 impl Guid {
+    #[must_use]
+    pub fn new(data1: u32, data2: u16, data3: u16, data4: [u8; 8]) -> Self {
+        Self(UA_Guid {
+            data1,
+            data2,
+            data3,
+            data4,
+        })
+    }
+
     #[must_use]
     pub const fn data1(&self) -> u32 {
         self.0.data1
@@ -28,14 +40,9 @@ impl Guid {
     #[must_use]
     #[cfg(feature = "uuid")]
     pub fn from_uuid(uuid: uuid::Uuid) -> Self {
-        use crate::DataType as _;
-        let mut guid = Guid::init();
         let (data1, data2, data3, data4) = uuid.as_fields();
-        guid.0.data1 = data1;
-        guid.0.data2 = data2;
-        guid.0.data3 = data3;
-        guid.0.data4 = *data4;
-        guid
+
+        Self::new(data1, data2, data3, *data4)
     }
 
     /// Converts the [`Guid`] into a [`Uuid`](uuid::Uuid).
@@ -43,7 +50,7 @@ impl Guid {
     /// The bitwise conversion might not result in a valid v1/v2/v3/v4/v5/v6/v7/v8 UUID.
     #[must_use]
     pub const fn to_uuid(&self) -> uuid::Uuid {
-        uuid::Uuid::from_fields(self.0.data1, self.0.data2, self.0.data3, &self.0.data4)
+        uuid::Uuid::from_fields(self.data1(), self.data2(), self.data3(), &self.data4())
     }
 }
 
